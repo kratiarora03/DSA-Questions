730717bc95f43c9e44fc2fcfd66d68fc5e58720c
Krati <krati21csu361@ncuindia.edu>
2024-06-08 14:22:54 +0530

    commit

--- Practice/containsDuplicate.cpp
+++ Practice/containsDuplicate.cpp
@@ -1,16 +1,25 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
-bool checkDuplicate(int arr , int n ){
-    if(i= 0 , i<=n , i++){
-        if(j= 0 , j<=n , j++){
-            arr[i] == arr[j] ; 
-            return true;
+
+bool checkDuplicate(int arr[], int n) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            if (arr[i] == arr[j]) {
+                return true;
+            }
         }
     }
+    return false;
 }
 
-int main(){
-    int arr[] = {1,2,3,1};
-    int n = 4; 
-     checkDuplicate(arr , n);
+int main() {
+    int arr[] = {1, 2, 3, 1};
+    int n = sizeof(arr) / sizeof(arr[0]); // calculate the size of the array
+    bool hasDuplicate = checkDuplicate(arr, n);
+    if (hasDuplicate) {
+        cout << "Duplicate found!" << endl;
+    } else {
+        cout << "No duplicates." << endl;
+    }
+    return 0;
 }
